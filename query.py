d8f103f37944db58f6773e88f70dcc563e3796e8
Alex Parsells <parsellsx@gmail.com>
2022-02-12 23:52:38 -0600

    Updating lat/long order

--- query.py
+++ query.py
@@ -39,7 +39,7 @@ def make_input_file(earth_lat,earth_long,start_time,stop_time):
     
     opp_mars_long = -354.656219
     opp_mars_lat = -2.328078
-    txtstring = "!$$SOF\nCOMMAND='g: " + str(opp_mars_long) + ", " + str(opp_mars_lat) + ", 0 @ 499'\nOBJ_DATA='NO'\nMAKE_EPHEM='YES'\nEPHEM_TYPE='V'\nCENTER='coord'\nREF_PLANE='ECLIPTIC'\nCOORD_TYPE='GEODETIC'\nSITE_COORD='" + str(earth_lat) + "," + str(earth_long) + ",0'\nSTART_TIME='" + start_time + "'\nSTOP_TIME='" + stop_time + "'\nSTEP_SIZE='1 day'\nREF_SYSTEM='ICRF'\nOUT_UNITS='KM-S'\nVEC_TABLE='3'\nVEC_CORR='NONE'\nCSV_FORMAT='YES'"
+    txtstring = "!$$SOF\nCOMMAND='g: " + str(opp_mars_long) + ", " + str(opp_mars_lat) + ", 0 @ 499'\nOBJ_DATA='NO'\nMAKE_EPHEM='YES'\nEPHEM_TYPE='V'\nCENTER='coord'\nREF_PLANE='ECLIPTIC'\nCOORD_TYPE='GEODETIC'\nSITE_COORD='" + str(earth_long) + "," + str(earth_lat) + ",0'\nSTART_TIME='" + start_time + "'\nSTOP_TIME='" + stop_time + "'\nSTEP_SIZE='1 day'\nREF_SYSTEM='ICRF'\nOUT_UNITS='KM-S'\nVEC_TABLE='3'\nVEC_CORR='NONE'\nCSV_FORMAT='YES'"
     with open('horizons_params.txt', 'w') as f:
         f.write(txtstring)
 
