07873c6536fbc5a24c09b22d5f76a550d6e41e82
aquinocosme <cosme.d.aquino.ovelar-1@ou.edu>
2022-02-12 18:23:28 -0600

    Finish putting inputs for horizon

--- extension.sh
+++ extension.sh
@@ -1,8 +1,23 @@
 #!/bin/sh
-#inputs target
-4
+#imports horizon
+telnet ssd.jpl.nasa.gov 6775
+#inputs opportunity target
+g: -354.656219,-2.328078,0 @ 499
+#Set ephemeris
 E
+#Sets vectors option
+v
+#Sets coordinates option
 coord
-g
--354.656219,-2.328078,0
+#Sets reference planes
 eclip
+#Sets Starting time
+2004-Jan-29 00:00
+#Sets End time
+2004-Feb-28 00:00
+#Sets frequency of observation
+1d
+#Confirms settings(yes)
+y
+#Gets new line
+N
